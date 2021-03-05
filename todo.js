a84742b57429f454b4a5cd2144401dc53cd4b257
“darcy504” <Darcyjdraper@gmail.com>
2021-03-05 17:46:18 -0600

    Added Event Listener, just need to finish JS

---
+++ todo.js
@@ -0,0 +1,9 @@
+let addToDoButton = document.getElementById('addtodo');
+let todoContainer = document.getElementById('todoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p'
+    todoContainer.appendChild(paragraph);
+})
+
